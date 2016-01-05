f1efdc8c443ca834494438abc410b175dbb47aac
Demo User <demouser@n242-205.mtholyoke.edu>
2016-01-05 08:21:53 -0800

    Added home page templates.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
@@ -12,10 +13,12 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        
+
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
         self.assertTrue(response.content.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.strip().endswith('</html>'))

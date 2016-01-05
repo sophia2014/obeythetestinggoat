bce40802dad7b1a8a2ac5124af942ebe611f520e
Demo User <demouser@n242-205.mtholyoke.edu>
2016-01-05 11:23:03 -0800

    Able to save a single to-do item.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -29,13 +29,11 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
 
-        input.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy peacock feathers' for row in rows)
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
 
         self.fail('Finish the app!')
 

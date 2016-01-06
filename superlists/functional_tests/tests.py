d3c87c82a676427503076de4a9e1d2d8cad10fda
Demo User <demouser@n242-205.mtholyoke.edu>
2016-01-06 10:34:36 -0800

    Migrated functional tests into own app.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
 
@@ -21,13 +21,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def enter_a_new_item(self, todo_text):
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys(todo_text)
         inputbox.send_keys(Keys.ENTER)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
 
@@ -50,6 +51,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main()

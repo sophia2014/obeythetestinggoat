30c69a267fe5dd111c68a6f3686a0e57eddf1e1e
Demo User <demouser@n242-205.mtholyoke.edu>
2016-01-12 11:16:07 -0800

    Refactor list view to handle adding items on POST.

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -82,14 +82,12 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other item 1')
         self.assertNotContains(response, 'other item 2')
 
-class NewItemTest(TestCase):
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' %(correct_list.id,),
+            '/lists/%d/' %(correct_list.id,),
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -98,13 +96,3 @@ class NewItemTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item' %(correct_list.id,),
-            data={'item_text': 'A new item for an existing list'}
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -22,12 +22,12 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_,})
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' %(list_.id,))
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+
+    # method == 'GET'
+    return render(request, 'list.html', {'list': list_,})
 
 def delete_item(request, list_id, item_id):
     list_ = List.objects.get(id=list_id)

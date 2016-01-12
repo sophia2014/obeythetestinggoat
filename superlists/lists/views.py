3a19df09e636b3cb92055da4fd042bb55ea59577
Demo User <demouser@n242-205.mtholyoke.edu>
2016-01-12 11:55:23 -0800

    Finally working

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -22,12 +22,18 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
 
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
 
     # method == 'GET'
-    return render(request, 'list.html', {'list': list_,})
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 def delete_item(request, list_id, item_id):
     list_ = List.objects.get(id=list_id)
